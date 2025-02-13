d0f9374ca2011aa894f1a82b5cab0902bfa78d5d
LeoEckert <leonard.eckert99@gmail.com>
2025-02-13 01:07:20 +0100

    fix für toggle

--- src/app/leaderboard/filters.tsx
+++ src/app/leaderboard/filters.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Select,
@@ -6,46 +8,67 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Switch } from "@/components/ui/switch";
 
-export function Filters() {
+interface FiltersProps {
+  hideSurrender: boolean;
+  onHideSurrenderChange: (value: boolean) => void;
+}
+
+export function Filters({
+  hideSurrender,
+  onHideSurrenderChange,
+}: FiltersProps) {
   return (
-    <div className='flex space-x-4'>
-      {/* Spiel Nummer */}
-      <Select>
-        <SelectTrigger className='w-[180px]'>
-          <SelectValue placeholder='Spiel Nr.' />
-        </SelectTrigger>
-        <SelectContent>
-          {Array.from({ length: 10 }, (_, i) => (
-            <SelectItem key={i} value={`${i + 1}`}>
-              Spiel {i + 1}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
+    <div className='flex flex-col space-y-4'>
+      {/* Zeile 1: Selektoren */}
+      <div className='flex space-x-4'>
+        <Select>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Spiel Nr.' />
+          </SelectTrigger>
+          <SelectContent>
+            {Array.from({ length: 10 }, (_, i) => (
+              <SelectItem key={i} value={`${i + 1}`}>
+                Spiel {i + 1}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
 
-      {/* Schwierigkeitsgrad */}
-      <Select>
-        <SelectTrigger className='w-[180px]'>
-          <SelectValue placeholder='Schwierigkeitsgrad' />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value='leicht'>Leicht</SelectItem>
-          <SelectItem value='mittel'>Mittel</SelectItem>
-          <SelectItem value='schwer'>Schwer</SelectItem>
-        </SelectContent>
-      </Select>
+        <Select>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Schwierigkeitsgrad' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='leicht'>Leicht</SelectItem>
+            <SelectItem value='mittel'>Mittel</SelectItem>
+            <SelectItem value='schwer'>Schwer</SelectItem>
+          </SelectContent>
+        </Select>
 
-      {/* Sprache */}
-      <Select>
-        <SelectTrigger className='w-[180px]'>
-          <SelectValue placeholder='Sprache' />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value='deutsch'>Deutsch</SelectItem>
-          <SelectItem value='englisch'>Englisch</SelectItem>
-        </SelectContent>
-      </Select>
+        <Select>
+          <SelectTrigger className='w-[180px]'>
+            <SelectValue placeholder='Sprache' />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value='deutsch'>Deutsch</SelectItem>
+            <SelectItem value='englisch'>Englisch</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {/* Zeile 2: Switch (z. B. "Surrender" ausblenden) */}
+      <div className='flex items-center space-x-2'>
+        <Switch
+          checked={hideSurrender}
+          onCheckedChange={(checked) => onHideSurrenderChange(checked)}
+          id='surrender-switch'
+        />
+        <label htmlFor='surrender-switch' className='text-sm font-medium'>
+          &quot;Surrender&quot; ausblenden
+        </label>
+      </div>
     </div>
   );
 }
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,14 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { Filters } from "./filters";
 import { LeaderboardTable } from "./LeaderboardTable";
 
 export default function Page() {
+  // State für das Ein-/Ausblenden von surrender
+  const [hideSurrender, setHideSurrender] = useState(false);
+
   return (
     <section className='mx-auto max-w-2xl px-4 py-6 space-y-8'>
-      <Filters />
-      <LeaderboardTable />
+      {/* Filters-Komponente mit Callback */}
+      <Filters
+        hideSurrender={hideSurrender}
+        onHideSurrenderChange={(value) => setHideSurrender(value)}
+      />
+
+      {/* Leaderboard-Tabelle, bekommt hideSurrender als Prop */}
+      <LeaderboardTable hideSurrender={hideSurrender} />
     </section>
   );
 }
