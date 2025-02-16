bc45ec14286b22e957dba135afb26aa0af066a58
David Kreismann <David.Kreismann@ibm.com>
2025-02-16 16:27:19 +0100

    update

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useMemo } from "react";
+import { useState, useMemo } from "react";
 import { Filters } from "@/components/leaderboard/filters";
 import { LeaderboardTable } from "@/components/leaderboard/leaderboardtable";
 
